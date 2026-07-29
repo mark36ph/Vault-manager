536bc2ff8a5aca5d99e5b880eb9392a8a80c25c3
Mark <mark36ph@gmail.com>
2026-07-29 19:33:03 +0100

    Add media library to app navigation

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import customtkinter as ctk
 
+from pages.media_library_page import MediaLibraryPage
 from widgets.asset_usage_tracking import install_asset_usage_tracking
 from ui.dashboard import Dashboard
 
@@ -8,4 +9,8 @@ ctk.set_default_color_theme("blue")
 install_asset_usage_tracking()
 
 app = Dashboard()
+app.add_sidebar_button(
+    "🖼 Media Library",
+    lambda: app.load_page(MediaLibraryPage, app),
+)
 app.mainloop()
