6fa2d1a5dc1ce9b5df619167bd5086f71f45fe62
Mark <mark36ph@gmail.com>
2026-08-13 20:16:13 +0100

    Apply compact Windows shell spacing on load

--- hybrid/FactVaultManager.Desktop/MainShellWindow.Layout.cs
+++ hybrid/FactVaultManager.Desktop/MainShellWindow.Layout.cs
@@ -4,7 +4,7 @@ namespace FactVaultManager.Desktop;
 
 public partial class MainShellWindow
 {
-    private void ApplyWindowsShellLayout()
+    private void MainShellWindow_LoadedLayout(object sender, RoutedEventArgs e)
     {
         MainTabs.Padding = new Thickness(0);
         MainTabs.Margin = new Thickness(12, 10, 14, 12);
