90537079ee27fc6c8f2a57b001d1419a5507ab43
Mark <mark36ph@gmail.com>
2026-08-13 21:27:54 +0100

    Finalize Explorer shell after render

--- hybrid/FactVaultManager.Desktop/MainShellWindow.ExplorerShell.cs
+++ hybrid/FactVaultManager.Desktop/MainShellWindow.ExplorerShell.cs
@@ -1,5 +1,12 @@
+using System;
+
 namespace FactVaultManager.Desktop;
 
 public partial class MainShellWindow
 {
+    protected override void OnContentRendered(EventArgs e)
+    {
+        base.OnContentRendered(e);
+        FinalizeExplorerNavigation();
+    }
 }
