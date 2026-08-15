b817077f735cbe0d70ce0a5d94a82ca02106474f
Mark <mark36ph@gmail.com>
2026-08-15 13:36:46 +0100

    Remove retired Python source and tests

--- image_providers/__init__.py
+++
@@ -1,8 +0,0 @@
-from image_providers.pexels import PexelsProvider
-from image_providers.pixabay import PixabayProvider
-
-
-__all__ = [
-    "PexelsProvider",
-    "PixabayProvider",
-]
--- image_providers/errors.py
+++
@@ -1,2 +0,0 @@
-class ImageSearchError(Exception):
-    """Raised when an image search or download cannot be completed."""
