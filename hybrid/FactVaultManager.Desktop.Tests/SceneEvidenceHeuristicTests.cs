e0f3b8ca176d6ffa570314ce8745c232113982ca
Mark <mark36ph@gmail.com>
2026-08-16 09:17:01 +0100

    Test generic multi-word scene evidence

--- hybrid/FactVaultManager.Desktop.Tests/SceneEvidenceHeuristicTests.cs
+++ hybrid/FactVaultManager.Desktop.Tests/SceneEvidenceHeuristicTests.cs
@@ -17,4 +17,44 @@ public sealed class SceneEvidenceHeuristicTests
                 "wombat droppings ground wildlife Australia",
                 "wombat"));
     }
+
+    [Fact]
+    public void SceneEvidencePhrase_PreservesBehaviorContext()
+    {
+        Assert.Equal(
+            "swimming underwater reef",
+            NativeVerifiedAssetAcquisitionEngine.SceneEvidencePhrase(
+                "sea turtle sea turtle swimming underwater reef",
+                "sea turtle"));
+
+        Assert.Equal(
+            "subject sea turtle swimming underwater reef",
+            NativeVerifiedAssetAcquisitionEngine.BuildEvidenceVerificationQuery(
+                "sea turtle sea turtle swimming underwater reef",
+                "sea turtle",
+                "swimming underwater reef"));
+    }
+
+    [Fact]
+    public void SceneEvidencePhrase_PreservesScientificContextButLeavesStandaloneTraceAlone()
+    {
+        Assert.Equal(
+            "blue blood protein",
+            NativeVerifiedAssetAcquisitionEngine.SceneEvidencePhrase(
+                "crab blue blood protein",
+                "crab"));
+
+        Assert.Equal(
+            "subject crab blue blood protein",
+            NativeVerifiedAssetAcquisitionEngine.BuildEvidenceVerificationQuery(
+                "crab blue blood protein",
+                "crab",
+                "blue blood protein"));
+
+        Assert.Equal(
+            "droppings",
+            NativeVerifiedAssetAcquisitionEngine.SceneEvidencePhrase(
+                "wombat droppings ground wildlife Australia",
+                "wombat"));
+    }
 }
