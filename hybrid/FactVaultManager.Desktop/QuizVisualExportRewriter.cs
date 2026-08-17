b2e9c0f20e9fe8e61bc7c4010e52f0634702feb1
Mark <mark36ph@gmail.com>
2026-08-17 21:34:18 +0100

    Render every second of quiz countdown

--- hybrid/FactVaultManager.Desktop/QuizVisualExportRewriter.cs
+++ hybrid/FactVaultManager.Desktop/QuizVisualExportRewriter.cs
@@ -17,6 +17,7 @@ public static class QuizVisualExportRewriter
         build.Timeline.Validate();
 
         QuizOpeningSequence.RenderAndApply(build.Timeline, build.ProjectFolder, options);
+        QuizFullCountdownRewriter.Apply(build.Timeline, options);
         QuizTimelineEndTrimmer.TrimToVideoEnd(build.Timeline);
         var finalized = QuizExportProjectFinalizer.Prepare(build);
         var metadata = new Dictionary<string, string>(StringComparer.OrdinalIgnoreCase)
