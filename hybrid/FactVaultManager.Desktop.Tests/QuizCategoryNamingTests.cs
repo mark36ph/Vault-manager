1bb05e0d4ff455d3b08cecd56282fa789565618b
Mark <mark36ph@gmail.com>
2026-08-18 19:34:18 +0100

    Add category quiz naming regression tests

---
+++ hybrid/FactVaultManager.Desktop.Tests/QuizCategoryNamingTests.cs
@@ -0,0 +1,16 @@
+namespace FactVaultManager.Desktop.Tests;
+
+public sealed class QuizCategoryNamingTests
+{
+    [Theory]
+    [InlineData(null, "General Knowledge Quiz")]
+    [InlineData("", "General Knowledge Quiz")]
+    [InlineData("Science", "Science Quiz")]
+    [InlineData("History", "History Quiz")]
+    [InlineData("Geography Quiz", "Geography Quiz")]
+    [InlineData("Movie Trivia", "Movie Trivia")]
+    public void SuggestSeriesName_UsesSelectedCategory(string? category, string expected)
+    {
+        Assert.Equal(expected, QuizPublishMetadataGenerator.SuggestSeriesName(category));
+    }
+}
