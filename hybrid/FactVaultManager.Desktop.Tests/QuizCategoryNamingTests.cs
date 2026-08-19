606b3f91c5a7b0cfaf3aa7cdb14b5581b11eba4c
Mark <mark36ph@gmail.com>
2026-08-19 17:10:09 +0100

    Test Film quiz series name

--- hybrid/FactVaultManager.Desktop.Tests/QuizCategoryNamingTests.cs
+++ hybrid/FactVaultManager.Desktop.Tests/QuizCategoryNamingTests.cs
@@ -8,6 +8,7 @@ public sealed class QuizCategoryNamingTests
     [InlineData("Science", "Science Quiz")]
     [InlineData("History", "History Quiz")]
     [InlineData("Music", "Music Quiz")]
+    [InlineData("Film", "Film Quiz")]
     [InlineData("Geography Quiz", "Geography Quiz")]
     [InlineData("Movie Trivia", "Movie Trivia")]
     public void SuggestSeriesName_UsesSelectedCategory(string? category, string expected)
