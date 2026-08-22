3fa9de21d376639ac7b32c9032b04b78c496b946
Mark <mark36ph@gmail.com>
2026-08-22 23:01:51 +0100

    Initialize Facebook Manager

--- hybrid/FactVaultManager.Desktop/MainShellWindow.ProjectsWorkflow.cs
+++ hybrid/FactVaultManager.Desktop/MainShellWindow.ProjectsWorkflow.cs
@@ -35,6 +35,7 @@ public partial class MainShellWindow
         InitializeQuizHistoryPage();
         InitializeQuizNotesPage();
         InitializeYouTubeAnalyticsPage();
+        InitializeFacebookAnalyticsPage();
         InitializeQuizDraftEditor();
         InitializeQuizRotationWorkflow();
         InitializeQuizExportWorkflow();
