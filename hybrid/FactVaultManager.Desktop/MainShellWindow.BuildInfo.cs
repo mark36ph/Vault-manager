d53183ab216d6a9ada0a31f9592530241c2379ce
Mark <mark36ph@gmail.com>
2026-08-26 22:24:42 +0100

    Show development build number in app title

---
+++ hybrid/FactVaultManager.Desktop/MainShellWindow.BuildInfo.cs
@@ -0,0 +1,26 @@
+using System.Windows;
+
+namespace FactVaultManager.Desktop;
+
+public partial class MainShellWindow
+{
+    public const int CurrentBuildNumber = 2;
+
+    private static readonly bool BuildInfoUiRegistered = RegisterBuildInfoUi();
+
+    private static bool RegisterBuildInfoUi()
+    {
+        EventManager.RegisterClassHandler(
+            typeof(MainShellWindow),
+            FrameworkElement.LoadedEvent,
+            new RoutedEventHandler(MainShellWindowBuildInfo_Loaded),
+            handledEventsToo: true);
+        return true;
+    }
+
+    private static void MainShellWindowBuildInfo_Loaded(object sender, RoutedEventArgs e)
+    {
+        if (sender is MainShellWindow window)
+            window.Title = $"FactVaultManager • Build {CurrentBuildNumber}";
+    }
+}
