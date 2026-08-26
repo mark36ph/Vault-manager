bfb4d4ed6b84026a45539a782a2f948d22d6060a
Mark <mark36ph@gmail.com>
2026-08-26 13:20:02 +0100

    Initialize thumbnail regeneration actions

--- hybrid/FactVaultManager.Desktop/App.xaml.cs
+++ hybrid/FactVaultManager.Desktop/App.xaml.cs
@@ -2,4 +2,15 @@ namespace FactVaultManager.Desktop;
 
 public partial class App : System.Windows.Application
 {
+    private bool _thumbnailRegenerationActionsInitialized;
+
+    protected override void OnActivated(EventArgs e)
+    {
+        base.OnActivated(e);
+        if (_thumbnailRegenerationActionsInitialized || MainWindow is not MainShellWindow shell)
+            return;
+
+        shell.InitializeUploadManagerThumbnailRegenerationActions();
+        _thumbnailRegenerationActionsInitialized = true;
+    }
 }
