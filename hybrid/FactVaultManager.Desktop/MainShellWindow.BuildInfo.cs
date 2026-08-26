9fbd36fb59b5e141aa65184c83de6f5193ada9d9
Mark <mark36ph@gmail.com>
2026-08-26 22:46:53 +0100

    Bump development marker to Build 5

--- hybrid/FactVaultManager.Desktop/MainShellWindow.BuildInfo.cs
+++ hybrid/FactVaultManager.Desktop/MainShellWindow.BuildInfo.cs
@@ -4,7 +4,7 @@ namespace FactVaultManager.Desktop;
 
 public partial class MainShellWindow
 {
-    public const int CurrentBuildNumber = 4;
+    public const int CurrentBuildNumber = 5;
 
     private static readonly bool BuildInfoUiRegistered = RegisterBuildInfoUi();
 
