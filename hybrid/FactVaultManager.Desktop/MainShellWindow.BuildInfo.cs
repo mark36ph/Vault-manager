902489f56ace937ba4b77b7c79ac6f18c97c9e7d
Mark <mark36ph@gmail.com>
2026-08-29 07:55:50 +0100

    Bump desktop build for data migration

--- hybrid/FactVaultManager.Desktop/MainShellWindow.BuildInfo.cs
+++ hybrid/FactVaultManager.Desktop/MainShellWindow.BuildInfo.cs
@@ -4,7 +4,7 @@ namespace FactVaultManager.Desktop;
 
 public partial class MainShellWindow
 {
-    public const int CurrentBuildNumber = 51;
+    public const int CurrentBuildNumber = 52;
 
     private static readonly bool BuildInfoUiRegistered = RegisterBuildInfoUi();
 
