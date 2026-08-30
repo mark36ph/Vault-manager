69abe61c536e688ff2281ba56550a6c7fc90d21d
Mark <mark36ph@gmail.com>
2026-08-30 22:54:45 +0100

    Bump Build 90 for faster archive matching

--- hybrid/FactVaultManager.Desktop/MainShellWindow.BuildInfo.cs
+++ hybrid/FactVaultManager.Desktop/MainShellWindow.BuildInfo.cs
@@ -4,7 +4,7 @@ namespace FactVaultManager.Desktop;
 
 public partial class MainShellWindow
 {
-    public const int CurrentBuildNumber = 89;
+    public const int CurrentBuildNumber = 90;
 
     private static readonly bool BuildInfoUiRegistered = RegisterBuildInfoUi();
 
