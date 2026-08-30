d5c21464bbe00b2c71895af856c6b92e1620a6a5
Mark <mark36ph@gmail.com>
2026-08-30 18:53:58 +0100

    Bump desktop app to Build 81

--- hybrid/FactVaultManager.Desktop/MainShellWindow.BuildInfo.cs
+++ hybrid/FactVaultManager.Desktop/MainShellWindow.BuildInfo.cs
@@ -4,7 +4,7 @@ namespace FactVaultManager.Desktop;
 
 public partial class MainShellWindow
 {
-    public const int CurrentBuildNumber = 80;
+    public const int CurrentBuildNumber = 81;
 
     private static readonly bool BuildInfoUiRegistered = RegisterBuildInfoUi();
 
