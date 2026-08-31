2a3182f91651b45eb0b7d890c8f11e4f28c0a3b9
Mark <mark36ph@gmail.com>
2026-08-31 18:25:28 +0100

    Bump desktop to Build 108

--- hybrid/FactVaultManager.Desktop/MainShellWindow.BuildInfo.cs
+++ hybrid/FactVaultManager.Desktop/MainShellWindow.BuildInfo.cs
@@ -4,7 +4,7 @@ namespace FactVaultManager.Desktop;
 
 public partial class MainShellWindow
 {
-    public const int CurrentBuildNumber = 107;
+    public const int CurrentBuildNumber = 108;
 
     private static readonly bool BuildInfoUiRegistered = RegisterBuildInfoUi();
 
