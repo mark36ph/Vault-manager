41577f46c85f2ea73faa11188964352d238ae39a
Mark <mark36ph@gmail.com>
2026-09-02 00:40:02 +0100

    Build 145: bump desktop build number

--- hybrid/FactVaultManager.Desktop/MainShellWindow.BuildInfo.cs
+++ hybrid/FactVaultManager.Desktop/MainShellWindow.BuildInfo.cs
@@ -4,7 +4,7 @@ namespace FactVaultManager.Desktop;
 
 public partial class MainShellWindow
 {
-    public const int CurrentBuildNumber = 144;
+    public const int CurrentBuildNumber = 145;
 
     private static readonly bool BuildInfoUiRegistered = RegisterBuildInfoUi();
 
