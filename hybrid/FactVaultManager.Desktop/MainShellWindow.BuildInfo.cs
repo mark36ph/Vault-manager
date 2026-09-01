d04f029e095df610b169d978f4465ba36a16b5a5
Mark <mark36ph@gmail.com>
2026-09-02 00:26:17 +0100

    Build 144: bump release metadata

--- hybrid/FactVaultManager.Desktop/MainShellWindow.BuildInfo.cs
+++ hybrid/FactVaultManager.Desktop/MainShellWindow.BuildInfo.cs
@@ -4,7 +4,7 @@ namespace FactVaultManager.Desktop;
 
 public partial class MainShellWindow
 {
-    public const int CurrentBuildNumber = 143;
+    public const int CurrentBuildNumber = 144;
 
     private static readonly bool BuildInfoUiRegistered = RegisterBuildInfoUi();
 
