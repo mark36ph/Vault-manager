afe8cf89c164c01b0e9ae6debcb82a8fadd9812f
Mark <mark36ph@gmail.com>
2026-09-01 21:10:59 +0100

    Build 137: bump desktop maintenance build

--- hybrid/FactVaultManager.Desktop/MainShellWindow.BuildInfo.cs
+++ hybrid/FactVaultManager.Desktop/MainShellWindow.BuildInfo.cs
@@ -4,7 +4,7 @@ namespace FactVaultManager.Desktop;
 
 public partial class MainShellWindow
 {
-    public const int CurrentBuildNumber = 136;
+    public const int CurrentBuildNumber = 137;
 
     private static readonly bool BuildInfoUiRegistered = RegisterBuildInfoUi();
 
