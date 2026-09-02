f0191d8cf9630f4d5ee007b82d5069f0987b720a
Mark <mark36ph@gmail.com>
2026-09-02 11:20:24 +0100

    Build 150: preserve current startup initializers

--- hybrid/FactVaultManager.Desktop/MainShellWindow.BuildInfo.cs
+++ hybrid/FactVaultManager.Desktop/MainShellWindow.BuildInfo.cs
@@ -1,60 +1,82 @@
+using System.Windows;
+
 namespace FactVaultManager.Desktop;
 
 public partial class MainShellWindow
 {
-    private const int CurrentBuildNumber = 150;
+    public const int CurrentBuildNumber = 150;
 
-    private void InitializeCurrentBuild()
+    private static readonly bool BuildInfoUiRegistered = RegisterBuildInfoUi();
+
+    private static bool RegisterBuildInfoUi()
     {
-        InitializeSettingsWorkflow();
-        InitializeApiConnectionsSettings();
-        InitializeApiConnectionsWebsite();
-        InitializeApiConnectionsYouTubeButton();
-        InitializeSettingsUiPolish();
-        InitializeQuizWorkflow();
-        InitializeQuizQuestionBankPage();
-        InitializeQuizQuestionBankBulkActions();
-        InitializeQuizQuestionImport();
-        InitializeQuizHistory();
-        InitializeQuizNotes();
-        InitializeQuizLibrary();
-        InitializeQuizArchiveDiagnosticsUi();
-        InitializeQuizProjectIdentity();
-        InitializeQuizAnalytics();
-        InitializeAutopilot();
-        InitializeWebsiteSettingsAdministration();
-        InitializeWebsiteReportedQuestions();
-        InitializeWebsiteUsers();
-        InitializeWebsiteCategoryManagement();
-        InitializeWebsiteRewards();
-        InitializeWebsiteInviteManagement();
-        InitializeYouTubeUpload();
-        InitializePublicationStatus();
-        InitializeInstagramManager();
-        InitializeFacebookManager();
-        InitializeYouTubeManager();
-        InitializeCreateAdvancedUiCleanup();
-        InitializeDailyUiCleanup();
-        InitializeQuizHistoryUiCleanup();
-        InitializeQuizOnlyCleanup();
-        InitializeStartupUiCleanupHotfix();
-        InitializeQuizQuestionBankUiCleanup();
-        InitializeQuizAnalyticsUiCleanup();
-        InitializeAutopilotShellActivationFix();
-        InitializeAutopilotNeedsYou();
-        InitializeAutopilotNeedsYouUiCleanup();
-        InitializeInstagramPageCleanup();
-        InitializeUploadManagerUiCleanup();
-        InitializeYouTubeManagerUiCleanup();
-        InitializeFacebookManagerUiCleanup();
-        InitializeWebsiteSettingsUiCleanup();
-        InitializeWebsiteManagementUiCleanup();
-        InitializeQuizHistoryPageCleanup();
-        InitializeQuizLibraryPageCleanup();
-        InitializeQuizQuestionBankPageCleanup();
-        InitializeQuizWorkflowPageCleanup();
-        InitializeQuizAnalyticsPageCleanup();
-        InitializeSettingsPageCleanup();
-        InitializeMainShellUiCleanup();
+        EventManager.RegisterClassHandler(
+            typeof(MainShellWindow),
+            FrameworkElement.LoadedEvent,
+            new RoutedEventHandler(MainShellWindowBuildInfo_Loaded),
+            handledEventsToo: true);
+        return true;
+    }
+
+    private static void MainShellWindowBuildInfo_Loaded(object sender, RoutedEventArgs e)
+    {
+        if (sender is MainShellWindow window)
+        {
+            window.Title = $"Factburst Quiz Manager • Build {CurrentBuildNumber}";
+            window.InitializeSettingsWorkflow();
+            window.InitializeApiConnectionsSettings();
+            window.InitializeApiConnectionsWebsite();
+            window.InitializeQuizOnlyCleanup();
+            window.FinalizeApiConnectionsYouTubeButton();
+            window.InitializeFinalVideoLabelSync();
+            window.InitializeQuizBatchButtonSync();
+            window.InitializeQuizYouTubePackagingMenuSync();
+            window.InitializeYouTubeUploadPackageUi();
+            window.InitializeUploadManagerYouTubeStatusSync();
+            window.InitializeUnifiedPublicationStateUi();
+            window.InitializeScheduledPromoPublishingBatchForApp();
+            window.InitializeScheduledRelatedVideoGuideForApp();
+            window.InitializeScheduledWebsitePublishingLayoutSafeForApp();
+            window.InitializeYouTubeGrowthAnalyticsUiReliably();
+            window.InitializeYouTubeGrowthRecommendationGuard();
+            window.InitializeYouTubeFirstCommentAutopilot();
+            window.InitializeFullAutopilot();
+            window.InitializeAutopilotFirstUi();
+            window.InitializeAutopilotMasterUi();
+            window.InitializeAutopilotNeedsYouTaskQueue();
+            window.InitializeAutopilotNeedsYouAlignedQueue();
+            window.InitializeAutopilotGuidedNeedsYou();
+            window.InitializeWebsiteManagerPage();
+            window.InitializeWebsiteYouTubeScheduleSync();
+            window.InitializeWebsiteVisibilityControls();
+            window.InitializeWebsiteUsersPage();
+            window.InitializeWebsiteAnalyticsPage();
+            window.InitializeWebsiteUserProvisioningControls();
+            window.InitializeWebsiteUsersFriendsPanel();
+            window.InitializeWebsiteMaintenancePlacement();
+            window.InitializeWebsiteAdministrationEnhancements();
+            window.InitializeWebsiteNavigationDivider();
+            window.InitializeWebsiteAdsSettings();
+            window.InitializeWebsiteSettingsShortcut();
+            window.InitializeWebsiteCommentModerationNavigation();
+            window.InitializeWebsiteSeoAuditPage();
+            window.InitializeWebsiteSeoAutoFixButton();
+            window.InitializeLogoQuizPromoArtworkRepair();
+            window.InitializeAutopilotNeedsYouCountSync();
+            window.InitializeInstagramPromoFollowup();
+            window.InitializeAutopilotNeedsYouVisualStability();
+            window.InitializeAutopilotShellActivationFix();
+            window.InitializeAutopilotScheduleTarget();
+            window.InitializeQuizHistoryBulkArchiveUi();
+            window.InitializeQuizHistoryGroupedBulkArchiveUi();
+            window.InitializeQuizHistoryUiCleanup();
+            window.InitializeQuizContentLifecycleUi();
+            window.InitializeLibraryPublicationStatusUi();
+            window.InitializeLibraryPlatformStatusFix();
+            window.InitializeLibraryPlatformSymbolFix();
+            window.InitializeStartupSafeUiCleanup();
+            window.InitializeCreateAdvancedUiCleanup();
+            window.InitializeDatabaseBackupAndRecovery();
+        }
     }
 }
