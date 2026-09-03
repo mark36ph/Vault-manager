c139f7039281c6b964372a264f45c5932d36010e
Mark <mark36ph@gmail.com>
2026-09-03 18:57:35 +0100

    Run quiz WPF rendering on a dedicated STA thread

---
+++ hybrid/FactVaultManager.Desktop/QuizRenderStaRunner.cs
@@ -0,0 +1,32 @@
+namespace FactVaultManager.Desktop;
+
+internal static class QuizRenderStaRunner
+{
+    public static Task<T> RunAsync<T>(Func<T> work)
+    {
+        ArgumentNullException.ThrowIfNull(work);
+
+        if (Thread.CurrentThread.GetApartmentState() == ApartmentState.STA)
+            return Task.FromResult(work());
+
+        var completion = new TaskCompletionSource<T>(TaskCreationOptions.RunContinuationsAsynchronously);
+        var thread = new Thread(() =>
+        {
+            try
+            {
+                completion.SetResult(work());
+            }
+            catch (Exception error)
+            {
+                completion.SetException(error);
+            }
+        })
+        {
+            IsBackground = true,
+            Name = "FactVaultManager Quiz Render STA",
+        };
+        thread.SetApartmentState(ApartmentState.STA);
+        thread.Start();
+        return completion.Task;
+    }
+}
