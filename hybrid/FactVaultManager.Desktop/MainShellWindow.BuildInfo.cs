fc2649fe4399456a6c64edd75ba1184e04926664
Mark <mark36ph@gmail.com>
2026-09-05 05:09:21 +0100

    Update desktop build marker to Build 165

--- hybrid/FactVaultManager.Desktop/MainShellWindow.BuildInfo.cs
+++ hybrid/FactVaultManager.Desktop/MainShellWindow.BuildInfo.cs
@@ -4,7 +4,7 @@ namespace FactVaultManager.Desktop;
 
 public partial class MainShellWindow
 {
-    public const int CurrentBuildNumber = 164;
+    public const int CurrentBuildNumber = 165;
 
     private static readonly bool BuildInfoUiRegistered = RegisterBuildInfoUi();
 
