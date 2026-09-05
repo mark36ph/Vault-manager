d15f69e71a92acb271f6762a1baa38f1947c4782
Mark <mark36ph@gmail.com>
2026-09-05 16:03:21 +0100

    Prepare Build 175 release

--- hybrid/FactVaultManager.Desktop/MainShellWindow.BuildInfo.cs
+++ hybrid/FactVaultManager.Desktop/MainShellWindow.BuildInfo.cs
@@ -6,7 +6,7 @@ namespace FactVaultManager.Desktop;
 
 public partial class MainShellWindow
 {
-    public const int CurrentBuildNumber = 174;
+    public const int CurrentBuildNumber = 175;
 
     private static readonly bool BuildInfoUiRegistered = RegisterBuildInfoUi();
     private bool _deferredShellInitializationScheduled;
