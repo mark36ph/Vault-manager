e736df91fe11fefb070a217882efd48c9e4d78ab
Mark <mark36ph@gmail.com>
2026-09-05 04:32:44 +0100

    Release Build 164: bump desktop build for published baseline

--- hybrid/FactVaultManager.Desktop/MainShellWindow.BuildInfo.cs
+++ hybrid/FactVaultManager.Desktop/MainShellWindow.BuildInfo.cs
@@ -4,7 +4,7 @@ namespace FactVaultManager.Desktop;
 
 public partial class MainShellWindow
 {
-    public const int CurrentBuildNumber = 163;
+    public const int CurrentBuildNumber = 164;
 
     private static readonly bool BuildInfoUiRegistered = RegisterBuildInfoUi();
 
