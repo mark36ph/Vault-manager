e868b8adf837cf0a6ab704e39796ead6764a5cdd
Mark <mark36ph@gmail.com>
2026-09-05 16:50:23 +0100

    Bump to Build 179 for faster menu navigation

--- hybrid/FactVaultManager.Desktop/MainShellWindow.BuildInfo.cs
+++ hybrid/FactVaultManager.Desktop/MainShellWindow.BuildInfo.cs
@@ -6,7 +6,7 @@ namespace FactVaultManager.Desktop;
 
 public partial class MainShellWindow
 {
-    public const int CurrentBuildNumber = 178;
+    public const int CurrentBuildNumber = 179;
 
     private static readonly bool BuildInfoUiRegistered = RegisterBuildInfoUi();
     private bool _deferredShellInitializationScheduled;
