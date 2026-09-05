32579a301addec69023e560a9909c98abf8d5e8c
Mark <mark36ph@gmail.com>
2026-09-05 14:15:46 +0100

    Bump desktop build to 168

--- hybrid/FactVaultManager.Desktop/MainShellWindow.BuildInfo.cs
+++ hybrid/FactVaultManager.Desktop/MainShellWindow.BuildInfo.cs
@@ -6,7 +6,7 @@ namespace FactVaultManager.Desktop;
 
 public partial class MainShellWindow
 {
-    public const int CurrentBuildNumber = 167;
+    public const int CurrentBuildNumber = 168;
 
     private static readonly bool BuildInfoUiRegistered = RegisterBuildInfoUi();
     private bool _deferredShellInitializationScheduled;
