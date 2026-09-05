ab7a37203c933c12b7f1b3087b505230ceddb80b
Mark <mark36ph@gmail.com>
2026-09-05 13:11:41 +0100

    Bump desktop build for 1.0.146 release

--- hybrid/FactVaultManager.Desktop/MainShellWindow.BuildInfo.cs
+++ hybrid/FactVaultManager.Desktop/MainShellWindow.BuildInfo.cs
@@ -6,7 +6,7 @@ namespace FactVaultManager.Desktop;
 
 public partial class MainShellWindow
 {
-    public const int CurrentBuildNumber = 166;
+    public const int CurrentBuildNumber = 167;
 
     private static readonly bool BuildInfoUiRegistered = RegisterBuildInfoUi();
     private bool _deferredShellInitializationScheduled;
