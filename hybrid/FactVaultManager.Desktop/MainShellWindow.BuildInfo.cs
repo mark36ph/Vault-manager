eed73e86c675acfa270b613235389adbb96cf861
Mark <mark36ph@gmail.com>
2026-09-05 21:08:04 +0100

    Bump build number to 191

--- hybrid/FactVaultManager.Desktop/MainShellWindow.BuildInfo.cs
+++ hybrid/FactVaultManager.Desktop/MainShellWindow.BuildInfo.cs
@@ -6,7 +6,7 @@ namespace FactVaultManager.Desktop;
 
 public partial class MainShellWindow
 {
-    public const int CurrentBuildNumber = 190;
+    public const int CurrentBuildNumber = 191;
 
     private static readonly bool BuildInfoUiRegistered = RegisterBuildInfoUi();
     private bool _deferredShellInitializationScheduled;
