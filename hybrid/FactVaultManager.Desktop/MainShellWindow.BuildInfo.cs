606de9ca74b4304c3902d0511b414df6b92af62e
Mark <mark36ph@gmail.com>
2026-09-05 16:21:33 +0100

    Bump desktop build for regression fix

--- hybrid/FactVaultManager.Desktop/MainShellWindow.BuildInfo.cs
+++ hybrid/FactVaultManager.Desktop/MainShellWindow.BuildInfo.cs
@@ -6,7 +6,7 @@ namespace FactVaultManager.Desktop;
 
 public partial class MainShellWindow
 {
-    public const int CurrentBuildNumber = 176;
+    public const int CurrentBuildNumber = 177;
 
     private static readonly bool BuildInfoUiRegistered = RegisterBuildInfoUi();
     private bool _deferredShellInitializationScheduled;
