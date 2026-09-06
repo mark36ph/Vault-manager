33c1fb71f4d63ca56edb1388f89b5a7820ba3613
Mark <mark36ph@gmail.com>
2026-09-06 15:00:05 +0100

    Bump build for updater hotfix

--- hybrid/FactVaultManager.Desktop/MainShellWindow.BuildInfo.cs
+++ hybrid/FactVaultManager.Desktop/MainShellWindow.BuildInfo.cs
@@ -6,7 +6,7 @@ namespace FactVaultManager.Desktop;
 
 public partial class MainShellWindow
 {
-    public const int CurrentBuildNumber = 197;
+    public const int CurrentBuildNumber = 198;
 
     private static readonly bool BuildInfoUiRegistered = RegisterBuildInfoUi();
     private bool _deferredShellInitializationScheduled;
