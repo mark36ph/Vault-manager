2938f54d7893b09a062c6b85d89867c7c336c7a6
Mark <mark36ph@gmail.com>
2026-09-06 17:07:19 +0100

    Bump build for Quiz History virtualization fix

--- hybrid/FactVaultManager.Desktop/MainShellWindow.BuildInfo.cs
+++ hybrid/FactVaultManager.Desktop/MainShellWindow.BuildInfo.cs
@@ -6,7 +6,7 @@ namespace FactVaultManager.Desktop;
 
 public partial class MainShellWindow
 {
-    public const int CurrentBuildNumber = 204;
+    public const int CurrentBuildNumber = 205;
 
     private static readonly bool BuildInfoUiRegistered = RegisterBuildInfoUi();
     private bool _deferredShellInitializationScheduled;
