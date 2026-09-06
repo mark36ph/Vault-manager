2592fee2ed8dc752e16ffbebe806b36992bc6aa4
Mark <mark36ph@gmail.com>
2026-09-06 18:00:32 +0100

    Bump build for current navigation profiler fix

--- hybrid/FactVaultManager.Desktop/MainShellWindow.BuildInfo.cs
+++ hybrid/FactVaultManager.Desktop/MainShellWindow.BuildInfo.cs
@@ -6,7 +6,7 @@ namespace FactVaultManager.Desktop;
 
 public partial class MainShellWindow
 {
-    public const int CurrentBuildNumber = 206;
+    public const int CurrentBuildNumber = 207;
 
     private static readonly bool BuildInfoUiRegistered = RegisterBuildInfoUi();
     private bool _deferredShellInitializationScheduled;
