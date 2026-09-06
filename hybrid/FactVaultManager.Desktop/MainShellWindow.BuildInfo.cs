dce3a8a484f8962a5db8fddb945b3a3a996bfd3f
Mark <mark36ph@gmail.com>
2026-09-06 20:00:50 +0100

    Bump desktop build for user account editing

--- hybrid/FactVaultManager.Desktop/MainShellWindow.BuildInfo.cs
+++ hybrid/FactVaultManager.Desktop/MainShellWindow.BuildInfo.cs
@@ -6,7 +6,7 @@ namespace FactVaultManager.Desktop;
 
 public partial class MainShellWindow
 {
-    public const int CurrentBuildNumber = 207;
+    public const int CurrentBuildNumber = 208;
 
     private static readonly bool BuildInfoUiRegistered = RegisterBuildInfoUi();
     private bool _deferredShellInitializationScheduled;
