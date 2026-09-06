129458b72ef2e75be72956c29526a3445b9cbe78
Mark <mark36ph@gmail.com>
2026-09-06 15:24:39 +0100

    Bump build for updater installer race fix

--- hybrid/FactVaultManager.Desktop/MainShellWindow.BuildInfo.cs
+++ hybrid/FactVaultManager.Desktop/MainShellWindow.BuildInfo.cs
@@ -6,7 +6,7 @@ namespace FactVaultManager.Desktop;
 
 public partial class MainShellWindow
 {
-    public const int CurrentBuildNumber = 199;
+    public const int CurrentBuildNumber = 200;
 
     private static readonly bool BuildInfoUiRegistered = RegisterBuildInfoUi();
     private bool _deferredShellInitializationScheduled;
