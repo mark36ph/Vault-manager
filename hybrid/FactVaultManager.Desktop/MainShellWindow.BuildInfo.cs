5c069cf0a24c7026ef06a670427415056fa6e15d
Mark <358519+mark36ph@users.noreply.github.com>
2026-09-07 20:42:15 +0100

    Bump build for restored full sidebar

--- hybrid/FactVaultManager.Desktop/MainShellWindow.BuildInfo.cs
+++ hybrid/FactVaultManager.Desktop/MainShellWindow.BuildInfo.cs
@@ -6,7 +6,7 @@ namespace FactVaultManager.Desktop;
 
 public partial class MainShellWindow
 {
-    public const int CurrentBuildNumber = 212;
+    public const int CurrentBuildNumber = 213;
 
     private static readonly bool BuildInfoUiRegistered = RegisterBuildInfoUi();
     private bool _deferredShellInitializationScheduled;
