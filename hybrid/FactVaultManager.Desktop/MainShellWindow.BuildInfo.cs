e575e24fccea13bbf2d0a2e0352ad292ccacd126
Mark <358519+mark36ph@users.noreply.github.com>
2026-09-07 21:32:51 +0100

    Bump to Build 215 for safe diagnostics profiling

--- hybrid/FactVaultManager.Desktop/MainShellWindow.BuildInfo.cs
+++ hybrid/FactVaultManager.Desktop/MainShellWindow.BuildInfo.cs
@@ -6,7 +6,7 @@ namespace FactVaultManager.Desktop;
 
 public partial class MainShellWindow
 {
-    public const int CurrentBuildNumber = 214;
+    public const int CurrentBuildNumber = 215;
 
     private static readonly bool BuildInfoUiRegistered = RegisterBuildInfoUi();
     private bool _deferredShellInitializationScheduled;
