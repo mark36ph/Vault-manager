68e0665856820af74883f8937dcf201fba679d0b
Mark <358519+mark36ph@users.noreply.github.com>
2026-09-09 20:11:17 +0100

    Bump desktop build for API backup fix

--- hybrid/FactVaultManager.Desktop/MainShellWindow.BuildInfo.cs
+++ hybrid/FactVaultManager.Desktop/MainShellWindow.BuildInfo.cs
@@ -6,7 +6,7 @@ namespace FactVaultManager.Desktop;
 
 public partial class MainShellWindow
 {
-    public const int CurrentBuildNumber = 218;
+    public const int CurrentBuildNumber = 219;
 
     private static readonly bool BuildInfoUiRegistered = RegisterBuildInfoUi();
     private bool _deferredShellInitializationScheduled;
