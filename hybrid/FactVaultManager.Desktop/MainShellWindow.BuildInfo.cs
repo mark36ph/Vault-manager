0439b068e01ba1d9fd2f2f73a0e56beadac70c08
Mark <358519+mark36ph@users.noreply.github.com>
2026-09-11 20:43:04 +0100

    Bump desktop build to 224 for automatic updater

--- hybrid/FactVaultManager.Desktop/MainShellWindow.BuildInfo.cs
+++ hybrid/FactVaultManager.Desktop/MainShellWindow.BuildInfo.cs
@@ -6,7 +6,7 @@ namespace FactVaultManager.Desktop;
 
 public partial class MainShellWindow
 {
-    public const int CurrentBuildNumber = 223;
+    public const int CurrentBuildNumber = 224;
 
     private static readonly bool BuildInfoUiRegistered = RegisterBuildInfoUi();
     private bool _deferredShellInitializationScheduled;
