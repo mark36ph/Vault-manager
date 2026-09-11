5fd7a5063dffe57ac693788a041176fee77093b0
Mark <358519+mark36ph@users.noreply.github.com>
2026-09-11 20:55:37 +0100

    Fix build metadata for next release

--- hybrid/FactVaultManager.Desktop/MainShellWindow.BuildInfo.cs
+++ hybrid/FactVaultManager.Desktop/MainShellWindow.BuildInfo.cs
@@ -6,7 +6,7 @@ namespace FactVaultManager.Desktop;
 
 public partial class MainShellWindow
 {
-    public const int CurrentBuildNumber = 224;
+    public const int CurrentBuildNumber = 226;
 
     private static readonly bool BuildInfoUiRegistered = RegisterBuildInfoUi();
     private bool _deferredShellInitializationScheduled;
